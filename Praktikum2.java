7dd2f9d3f3f15b0b6a99209f7afdf35f32171fd7
fidi <afdhalfidi.ansori015@gmail.com>
2025-08-30 10:06:11 +0700

    Add files via upload

---
+++ Praktikum2.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikum2;
+/**
+ *
+ * @author Afdhal Fidi Ansori
+ */
+public class Praktikum2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        /*
+        menampilkan nama
+        */
+        System.out.println("nama saya fidi");
+        
+        int umur;
+        umur = 19;
+        
+        String nama = "Afdhal Fidi Ansori";
+        
+        System.out.println("nama saya " + nama);
+        System.out.println("umur saya " + umur);
+        
+        int x = 1, y = 3, z = 5;
+        System.out.println(x + " " + y + " " + z);
+        
+        double[] ipSemester = {3.5, 3.7, 3.9, 4.0};
+
+        System.out.println("ip semester 4 saya adalah : " + ipSemester[3]);
+          
+        int intSemester1 = (int) ipSemester[0];
+        System.out.println("ip semester 1 saya adalah : " + intSemester1);
+        
+        long nilaiSangatbesar = 3000000000L;
+        int intnilaiSangatbesar = (int) nilaiSangatbesar;
+        
+        System.out.println("nilai long yang belum diubah ke int : " + nilaiSangatbesar);
+        System.out.println("nilai long yang sudah diubah ke int : " + intnilaiSangatbesar);
+        
+        final double PI = 3.14;
+        //PI = 3.5;
+        System.out.println("Nilai PI = " + PI);
+    }
+}
